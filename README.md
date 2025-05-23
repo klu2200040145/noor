bce1ccb71eaba18196b65609b976992bb4de9155
SHAIK NOORUDDIN <144267152+klu2200040145@users.noreply.github.com>
2025-05-23 11:15:05 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# noor
+
+A custom Python library by Shaik Nooruddin for simple operations.
+
+## Installation
+
+```bash
+pip install git+https://github.com/klu2200040145/noor.git
+```
+
+## Usage
+
+```python
+from noor import add
+
+print(add(3, 4))  # Output: 7
+```
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='noor',
+    version='0.1',
+    packages=find_packages(),
+    description='A custom library by Shaik Nooruddin for simple math functions',
+    author='Shaik Nooruddin',
+    author_email='youremail@example.com',
+    url='https://github.com/klu2200040145/noor',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+)
